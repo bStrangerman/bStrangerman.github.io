361acc588038d6648caac678cf9668b53fe92cc1
Brandon Haberman <brandonhaberman@gmail.com>
2018-12-15 09:24:32 -0500

    start of mailing the contact form

---
+++ contact_submit.php
@@ -0,0 +1,12 @@
+<?php
+
+$information = $_POST;
+
+$fname = $information['fname'];
+$lname = $information['lname'];
+$email = $information['email'];
+$subject = $information['subject'];
+$message = $information['message'];
+
+// Insert mailing function here
+?>
