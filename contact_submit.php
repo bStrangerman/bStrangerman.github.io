5a7e7132c2a8ba5918fb32a5f379401072ebb06a
Brandon Haberman <brandonhaberman@gmail.com>
2018-12-16 22:53:16 -0800

    emails are locally sending

--- contact_submit.php
+++ contact_submit.php
@@ -12,6 +12,21 @@ $message = $information['message'];
 // Insert mailing function here
 
 if(true){
+  $emailContent = "<strong>Your Name: </strong>" . $fname . " " . $lname . "<br>";
+  $emailContent .= "<strong>Your Email: </strong>" . $email . "<br>";
+  $emailContent .= "<strong>Your Message: </strong>";
+  $emailContent .= "<br><strong>" . $subject . "</strong><br>";
+  $emailContent .= $message . "<br>";
+
+  $recipient="username@localhost";
+  $subject="New Contact from brandonhaberman@gmail.com";
+  $mail_body= $emailContent;
+  $headers = "From: " . strip_tags($email) . "\r\n";
+  $headers .= "Reply-To: ". strip_tags($recipient) . "\r\n";
+  $headers .= "MIME-Version: 1.0\r\n";
+  $headers .= "Content-Type: text/html; charset=ISO-8859-1\r\n";
+  echo mail($recipient, $subject, $mail_body, $headers);
+
   $newmessage = "Success!";
 }
 else{
